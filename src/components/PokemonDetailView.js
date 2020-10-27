29b7b0c3d6ba1d020c5c454d1d30f2fcb9bb9a5b
Crlbrwrth <carl.beerwerth@gmail.com>
2020-10-27 01:37:48 +0100

    styling

--- src/components/PokemonDetailView.js
+++ src/components/PokemonDetailView.js
@@ -21,41 +21,43 @@ const PokemonDetailView = () => {
   }, [])
 
   return (
-    <div className='detail-view-container'>
-      <div className='pokemon-detail-view'>
-        <div className='close-btn' onClick={handleCloseView}>X</div>
-        <img src={pokemon.sprites.front_default} alt='front_default' className='top left' />
-        <img src={pokemon.sprites.back_default} alt='back_default' className='bottom right' />
-        <img src={pokemon.sprites.front_shiny} alt='front_shiny' className='top right' />
-        <img src={pokemon.sprites.back_shiny} alt='back_shiny' className='bottom left' />
-        <div className='controls'>
-          <span>&lt;</span><span>&nbsp;#{pokemon.id}&nbsp;</span><span>&gt;</span>
+    <div className='pokemon-detail-view'>
+      <div className='close-btn' onClick={handleCloseView}>X</div>
+      <img src={pokemon.sprites.front_default} alt='front_default' className='top left' />
+      <img src={pokemon.sprites.back_default} alt='back_default' className='bottom right' />
+      <img src={pokemon.sprites.front_shiny} alt='front_shiny' className='top right' />
+      <img src={pokemon.sprites.back_shiny} alt='back_shiny' className='bottom left' />
+      <div className='controls'>
+        <span>&lt;</span><span>&nbsp;#{pokemon.id}&nbsp;</span><span>&gt;</span>
+      </div>
+      <h2>{pokemon.name}</h2>
+      <TypeLabels types={pokemon.types} />
+      <div className='overview'>
+        <div>
+          <div><b>Pokedex No</b></div>
+          <div>{pokemon.id}</div>
         </div>
-        <h2>{pokemon.name}</h2>
-        <TypeLabels types={pokemon.types} />
-        <div className='overview'>
-          <div>
-            <div><b>Pokedex No</b></div>
-            <div>{pokemon.id}</div>
-          </div>
-          <div>
-            <div><b>Height</b></div>
-            <div>{pokemon.height}</div>
-          </div>
-          <div>
-            <div><b>Weight</b></div>
-            <div>{pokemon.weight}</div>
-          </div>
+        <div>
+          <div><b>Height</b></div>
+          <div>{pokemon.height}</div>
+        </div>
+        <div>
+          <div><b>Weight</b></div>
+          <div>{pokemon.weight}</div>
         </div>
       </div>
       {/* Bild mit mehreren Quellen externe funktion fallback bild */}
       {
-        pokemon.stats.map(stat => (
-          <div key={stat.stat.name} className='stat-item'>
-            <div>{stat.stat.name}</div>
-            <div>{stat.base_stat}</div>
-          </div>
-        ))
+        <div className='stats-container'>
+          {
+            pokemon.stats.map(stat => (
+              <div key={stat.stat.name} className='stat-item'>
+                <div>{stat.stat.name}</div>
+                <div>{stat.base_stat}</div>
+              </div>
+            ))
+          }
+        </div>
       }
     </div>
   )
