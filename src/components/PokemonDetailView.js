f443d25f7dd43fee28c6cbfb13666f3a52476b3b
Crlbrwrth <carl.beerwerth@gmail.com>
2020-10-27 01:27:17 +0100

    styling

--- src/components/PokemonDetailView.js
+++ src/components/PokemonDetailView.js
@@ -8,9 +8,10 @@ const PokemonDetailView = () => {
   const dispatch = useDispatch()
   const pokemon = useSelector(state => state.pokedex.activePokemon)
   const handleKeyDown = (event) => {
-    event.keyCode === 27 &&
-      dispatch(pokedexSlice.actions.setActive())
+    event.keyCode === 27 && handleCloseView()
   }
+
+  const handleCloseView = () => dispatch(pokedexSlice.actions.setActive())
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown)
@@ -20,31 +21,40 @@ const PokemonDetailView = () => {
   }, [])
 
   return (
-    <div className='pokemon-detail-view'>
-      <img src={pokemon.sprites.front_default} alt='front_default' className='top left' />
-      <img src={pokemon.sprites.back_default} alt='back_default' className='bottom left' />
-      <img src={pokemon.sprites.front_shiny} alt='front_shiny' className='top right' />
-      <img src={pokemon.sprites.back_shiny} alt='back_shiny' className='bottom right' />
-      <h2>{pokemon.name}</h2>
-      <TypeLabels types={pokemon.types} />
-      <div className='overview'>
-        <div>
-          <div><b>Pokedex No</b></div>
-          <div>{pokemon.id}</div>
+    <div className='detail-view-container'>
+      <div className='pokemon-detail-view'>
+        <div className='close-btn' onClick={handleCloseView}>X</div>
+        <img src={pokemon.sprites.front_default} alt='front_default' className='top left' />
+        <img src={pokemon.sprites.back_default} alt='back_default' className='bottom right' />
+        <img src={pokemon.sprites.front_shiny} alt='front_shiny' className='top right' />
+        <img src={pokemon.sprites.back_shiny} alt='back_shiny' className='bottom left' />
+        <div className='controls'>
+          <span>&lt;</span><span>&nbsp;#{pokemon.id}&nbsp;</span><span>&gt;</span>
         </div>
-        <div>
-          <div><b>Height</b></div>
-          <div>{pokemon.height}</div>
-        </div>
-        <div>
-          <div><b>Weight</b></div>
-          <div>{pokemon.weight}</div>
+        <h2>{pokemon.name}</h2>
+        <TypeLabels types={pokemon.types} />
+        <div className='overview'>
+          <div>
+            <div><b>Pokedex No</b></div>
+            <div>{pokemon.id}</div>
+          </div>
+          <div>
+            <div><b>Height</b></div>
+            <div>{pokemon.height}</div>
+          </div>
+          <div>
+            <div><b>Weight</b></div>
+            <div>{pokemon.weight}</div>
+          </div>
         </div>
       </div>
       {/* Bild mit mehreren Quellen externe funktion fallback bild */}
       {
         pokemon.stats.map(stat => (
-          <div key={stat.stat.name}>{stat.stat.name}: {stat.base_stat}</div>
+          <div key={stat.stat.name} className='stat-item'>
+            <div>{stat.stat.name}</div>
+            <div>{stat.base_stat}</div>
+          </div>
         ))
       }
     </div>
